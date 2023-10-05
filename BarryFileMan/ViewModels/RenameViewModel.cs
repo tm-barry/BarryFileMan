d0b9ff2a21902a06134a5c39a7ea100aa5de28d8
tm-barry <travis.m.barry@gmail.com>
2023-10-04 19:47:18 -0500

    add draggle listbox

--- BarryFileMan/ViewModels/RenameViewModel.cs
+++ BarryFileMan/ViewModels/RenameViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,18 @@ namespace BarryFileMan.ViewModels
 {
     public partial class RenameViewModel : ViewModelBase
     {
+        [ObservableProperty]
+        private ObservableCollection<string> _files = new();
+
+        [ObservableProperty]
+        private string? _selectedFile;
+
+        public RenameViewModel()
+        {
+            for(int i = 0; i <= 1000; i++)
+            {
+                Files.Add(i.ToString());
+            }
+        }
     }
 }
